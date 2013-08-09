45410537fe2418c31581601a2cd24df614cf0ecd
Stephanie Datu <stephdatu@gmail.com>
2013-08-08 22:56:19 -0700

    Updating app to use simple_form gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 LunchTracker::Application.routes.draw do
   resources :lunches
 
+  root to: "lunches#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
